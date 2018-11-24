875f55c160797b0abeee399ec520e5cbf1d0dcf0
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-24 23:08:57 +0100

    add file

---
+++ edit_account.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'structure/template.php';
+?>
+<?php echo $template_start; ?>
+    <main>
+        <?php echo $template_nav; ?>
+        <section id='edAccount'>
+            <h1>Edycja Konta</h1>
+            <p>edytujemy konto</p>
+        </section>
+        <?php echo $template_footer; ?>
+    </main>
+<?php echo $template_end; ?>

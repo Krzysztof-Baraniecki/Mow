8cccb8dbdffd0e433dcee388abb0b69cc2d2500f
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-25 21:30:32 +0100

    added statistics for subpage 'program'

--- program.php
+++ program.php
@@ -6,8 +6,16 @@ require_once 'structure/template.php';
         <?php echo $template_nav; ?>
         <section id='program'>
             <h1>Program24</h1>
-            Strona z myślą o dyrekcji <br />
-            {lista dodawanych wszystkich punktów oraz ocen}:<br />
+
+<div class="row text-center statistics">
+    <div class="col-12 text-left font-weight-bold"><h2>Dane:</h2></div>
+    <div class="col-4"><p>Dzieci w ośrodku: <span class="font-weight-bold">86</span></p></div>
+    <div class="col-4"><p>Dzieci obecnych: <span class="font-weight-bold">80</span></p></div>
+    <div class="col-4"><p>Dzieci nie obecnych: <span class="font-weight-bold">6</span></p></div>
+</div>
+
+            <br /><br />
+            {lista dodawanych wszystkich punktów oraz ocen z okresu 3 tygodni}:<br />
             -Imie nazwisko(podopiecznego) | płęć | gr | ilość punktów/ocena tygodniowa lub ocena mieścieczna | krótyki opis kączący się "..." | kto wystawił | data wystawienia <br /> 
             
         </section>

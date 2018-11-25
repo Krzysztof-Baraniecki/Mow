5ee64ee761f9aa56a111449e0d4406d3ca8c63ff
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-25 22:02:07 +0100

    description of functionality in edit program

--- edit_program.php
+++ edit_program.php
@@ -6,7 +6,18 @@ require_once 'structure/template.php';
         <?php echo $template_nav; ?>
         <section id='edProgram'>
             <h1>Edycja Programu24</h1>
-            <p>Nowa strona</p>
+            {dodaj nową kfalifikacje pkt} <br />
+            lista obcencyh kfalifikacji:<br />
+            lp. | Opis skrócony | ilość punktów | {dodatkowe funkcjie jak obniżenie oceny do nagannej} [ po kliknieciu pojawia sie popup z edycją lub usunięciem w tym znajduje się nazwa opis ilość punktów funkcjie] <br />
+            <br />
+            {(?)dodaj ocene tygodniową}<br />
+            Lista ocen tygodniowych: <br />
+            lp. Nazwa | Przedział punktów [ po kliknieciu popup - nazwa | przedział punktów | przywileje | ostatnia osoba edytująca  {usuń}] <br/>
+            <br />
+            {(?)dodaj ocene miesięczną}<br />
+            Lista ocen miesięcznych: <br />
+            lp. Nazwa | Przedział punktów [ po kliknieciu popup - nazwa | przedział punktów | przywileje | ostatnia osoba edytująca  {usuń}] <br/>
+
         </section>
         <?php echo $template_footer; ?>
     </main>

7b47cbd8cc4112b7ea35ab89f14f08e0a30a79ad
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-25 19:59:53 +0100

    add file

---
+++ edit_ groups.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'structure/template.php';
+?>
+<?php echo $template_start; ?>
+    <main>
+        <?php echo $template_nav; ?>
+        <section id='edGroups'>
+            <h1>Edycja Grup</h1>
+            <p>Nowa strona</p>
+        </section>
+        <?php echo $template_footer; ?>
+    </main>
+<?php echo $template_end; ?>

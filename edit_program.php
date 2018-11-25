71b95875aef26d704a253edadaaa4df6e1273125
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-25 21:33:50 +0100

    add file for subpage ' edit program'

---
+++ edit_program.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'structure/template.php';
+?>
+<?php echo $template_start; ?>
+    <main>
+        <?php echo $template_nav; ?>
+        <section id='edProgram'>
+            <h1>Edycja Programu24</h1>
+            <p>Nowa strona</p>
+        </section>
+        <?php echo $template_footer; ?>
+    </main>
+<?php echo $template_end; ?>

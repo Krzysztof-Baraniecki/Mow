2178f8ef8c55f168cc60f37656b67ba994a9f307
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-25 20:18:02 +0100

    description of functionality edit users

--- edit_user.php
+++ edit_user.php
@@ -6,7 +6,7 @@ require_once 'structure/template.php';
         <?php echo $template_nav; ?>
         <section id='edUsers'>
             <h1>Edytuj urzytykowników</h1>
-            {lista uzytnowników}
+            {lista uzytnowników} - [po kliknieciu pojawia się pupup - z edycją Imienia Nazwiska emaila telefonu zdjęcia {tabela uprawnień} | usuń urzytykownika(archiwizowanie)]
         </section>
         <?php echo $template_footer; ?>
     </main>
